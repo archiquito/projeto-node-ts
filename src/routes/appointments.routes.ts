3e49315e67355ed73b5326b8db572bbe73330195
archiquito <archiquito@gmail.com>
2020-10-15 12:52:30 -0300

    model appointment

--- src/routes/appointments.routes.ts
+++ src/routes/appointments.routes.ts
@@ -1,20 +1,28 @@
 import { Router } from 'express';
-import { uuid } from 'uuidv4';
+import { startOfHour, parseISO, isEqual } from 'date-fns';
+import Appointment from '../models/Appointment';
 
 const appointmentsRouter = Router();
 
-const appointments = [];
+const appointments: Appointment[] = [];
 
 appointmentsRouter.post('/', (request, response) => {
-const { provider, date } = request.body
+  const { provider, date } = request.body;
+  const parsedDate = startOfHour(parseISO(date));
 
-const appointment = {
-  id: uuid(),
-  provider,
-  date
-}
+  const findSameDate = appointments.find(appointment =>
+    isEqual(parsedDate, appointment.date),
+  );
 
-appointments.push(appointment )
+  if (findSameDate) {
+    return response
+      .status(400)
+      .json({ message: 'This appointment is already booked!' });
+  }
+
+  const appointment = new Appointment(provider, parsedDate)
+
+  appointments.push(appointment);
   return response.json(appointment);
 });
 
